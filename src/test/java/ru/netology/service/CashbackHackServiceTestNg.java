b25264503e76050ee306f37d04282e6d2879798b
zaracortez <zarema.cortez@yandex.ru>
2023-01-07 23:05:19 +0300

    Added tests

--- src/test/java/ru/netology/service/CashbackHackServiceTestNg.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTestNg.java
@@ -9,13 +9,44 @@ public class CashbackHackServiceTestNg {
 
     @Test
     public void adviceToBuyMore() {
-        CashbackHackService service = new CashbackHackService();
+        CashbackHackServiceNg serviceNg = new CashbackHackServiceNg();
         int amount = 900;
         int expected = 100;
-        int actual = service.remain(900);
+        int actual = serviceNg.remain(900);
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testRemain() {
+        CashbackHackServiceNg serviceNg = new CashbackHackServiceNg();
+        int amount = 700;
+        int expected = 300;
+        int actual = serviceNg.remain(700);
         assertEquals(actual, expected);
 
-//    @Test
-  //  public void testRemain() {
     }
+    @Test
+    public void buyNotEqualsBoundary(){
+        CashbackHackServiceNg serviceNg = new CashbackHackServiceNg();
+        int amount = 1750;
+        int expected = 250;
+        int actual = serviceNg.remain(1750);
+        assertEquals(actual,expected );
+    }
+    @Test
+    public void buyThousand(){
+        CashbackHackServiceNg serviceNg = new CashbackHackServiceNg();
+        int amount = 1000;
+        int expected =1000;
+        int actual = serviceNg.remain(1000);
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void buyEqualsBoundary() {
+        CashbackHackServiceNg serviceNg = new CashbackHackServiceNg();
+        int amount = 3000;
+        int expected = 1000;
+        int actual = serviceNg.remain(3000);
+        assertEquals(actual, expected);
+    }
+
 }
